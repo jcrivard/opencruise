615a9a1817fe0b98d01d56aa62b70d8f77459f58
jcrivard <jcrivard@mtu.edu>
2017-04-12 08:50:53 -0400

    Finish help screen!

--- src/components/help.vue
+++ src/components/help.vue
@@ -4,29 +4,29 @@
     <div class="app-content">
         <div class="help-collapsible" role="button" @click="getAnimation('group1')">
             <i class="material-icons">format_list_numbered</i>
-            <span>Support and Installation</span>
+            <span>Installation</span>
         </div>
         <div class="help-collapsible-container">
             <div class="help-collapsible-group slide-up" role="group" ref="group1">
                 <div>
-                    <h4>Mobile Support</h4>
-                    <ul>
-                        <li><span class="help-item">Chrome 57+ (Android):</span> tap the three dot menu and then "Add to Home Screen".</li>
-                        <li><span class="help-item">Firefox 53+ (Android):</span> tap the three dot menu, then page, then "Add to Home Screen."</li>
+                    <h4>Android</h4>
+                    <ul class="collapse help-left">
+                        <li><span class="help-item">Chrome 57+</span> - tap the three dot menu and then "Add to Home Screen".</li>
+                        <li><span class="help-item">Firefox 52+</span> - tap the three dot menu, then page, then "Add to Home Screen."</li>
                         </li>
-
                     </ul>
-                    <h4>Desktop Support</h4>
-                    <ul>
-                        <li><span class="help-item">Chrome 57+ (any platform)</span></li>
-                        <li><span class="help-item">Firefox 53+ (any platform)</span></li>
+                    <h4>Desktop</h4>
+                    <ul class="collapse help-left">
+                        <li><span class="help-item">Chrome 57+</span> - tap the three dot menu and then "More Tools" then "Add to Desktop".</li>
+                        <li><span class="help-item">Firefox 52+</span> - create a bookmark, view all bookmarks, then drag the new bookmark
+                        to the desktop."</li>
                     </ul>
                     <h4>Browser Settings</h4>
-                        <ul>
-                            <li>Javascript must be enabled.</li>
-                            <li>Respond "Allow" to prompts for notifications and geolocation when prompted.</li>
-                            <li>Incognito or private browsing will not work and data will NOT BE SAVED!</li>
-                            <li>The application uses persistent storage, but to be
+                        <ul class="collapse help-left">
+                            <li><span class="help-item">Javascript</span> - must be enabled.</li>
+                            <li><span class="help-item">Geolocation/Notifications</span> - Respond "Allow" to prompts when prompted.</li>
+                            <li><span class="help-item">Incognito/Private Browsing</span> - will not work and data will NOT BE SAVED!</li>
+                            <li><span class="help-item">Deleting Cache</span> - the application uses persistent storage, but to be
                                 safe it is suggested to save all collected data before clearing the cache.
                             </li>
                         </ul>
@@ -127,23 +127,19 @@
         <div class="help-collapsible-container">
             <div class="help-collapsible-group slide-up" role="group" ref="group7">
                 <div>
-                    <h4>Statisticss</h4>
-                    <ul id="helpConfig" class="collapse help-left">
-                        <li><span class="help-item">Interval -</span> XXXXXXXXXXXXXXXXXXXXX.</li>
-                        <li><span class="help-item">Default Data Fields -</span> these are the global defaults and can be changed later for each cruise.
-                            The field names are the title(s) on the plot entry screen.
-                            Min and max values are used during plot entry to control field colors (red is out of range, green
-                            is in range).  Min field2 for field3 is intended to be a limit check for sawtimber.  Assuming field2
-                            is DBH and field3 is a measure of Sawtimber, this value indicates the minimum diameter that a tree
-                            can be to have sawtimber.  If a tree with a smaller diameter is entered and the user enters a value
-                            greater than zero for sawtimber, the sawtimber field will turn red.</li>
-                        <li><span class="help-item">Species codes -</span> changes made here affect all of the species dropdowns used during
-                        data entry.  If you remove a species used in an existing cruise, the data itself will not be affected, but you will not be
-                        able to see the deleted species on any screens. To change the order of species in the dropdowns, swap codes so that more preferred
-                        species are towards the top of the list.</li>
-                        <li><span class="help-item">Grade codes -</span> add, remove or change grades used when doing segment entry.</li>
-                        <li><span class="help-item">BAF -</span> add or remove BAFs as needed.  It is not recommended to use more than 5 on devices
-                        with smaller screens.</li>
+                    <ul class="collapse help-left">
+                        <li><span class="help-item">Interval -</span> confidence limits.  eg. 10 represents a confidence interval
+                        of +/- 10%.</li>
+                        <li><span class="help-item">Level -</span> confidence level.  eg. 95% means that there is a 95% probability
+                        that the mean lies within the confidence interval.</li>
+                        <li><span class="help-item">Include/Exclude Species -</span> to include or exclude certain species from the
+                        calculations, select/deselect values.  When changing one field (ie. Include Species) the other field (ie.
+                        Exclude Species) will automatically update to reflect the values not included in the other field.  The default is
+                        for all species to be included in the calculations.</li>
+                        <li><span class="help-item">Exclude Example -</span>if you are recording snags as part of your cruise, but do not
+                        want to include them in the calculations, select SNAG in the Exclude Species list.</li>
+                        <li><span class="help-item">Include Example -</span>if you want to know how much Yellow Birch is in your stand,
+                        select only YB in the Include Species list.</li>
                     </ul>
                 </div>
             </div>
@@ -155,7 +151,8 @@
         <div class="help-collapsible-container">
             <div class="help-collapsible-group  slide-up" role="group" ref="group8">
                 <ul id="helpDownload" class="collapse">
-                    <li>Tap "Download" in the pullout menu.</li>
+                    <li>Tap "Download" in the cruisemenu to download a cruise.  On Android/Windows/Linux the file will be downloaded to
+                        the "Downloads" folder configured for your device.</li>
                 </ul>
             </div>
         </div>
@@ -165,10 +162,9 @@
         </div>
         <div class="help-collapsible-container">
             <div class="help-collapsible-group  slide-up" role="group" ref="group9">
-                <ul id="helpOffline" class="collapse">
-                    <li>Visit website, or open application from home screen (see Browser Notes) when you have network access.</li>
-                    <li>Go into "Airplane Mode" to save battery.</li>
-                    <li>When done collecting data, exit airplane mode.</li>
+                <ul id="helpOffline" class="collapse help-left">
+                    <li><span class="help-item">Load application -</span>when you visit the website, or select the homescreenVisit website, or open application from home screen when you have network access.</li>
+                    <li><span class="help-item">Airplace Mode -</span>use to turn off WIFI/LTE and save battery.</li>
                 </ul>
             </div>
         </div>
@@ -178,21 +174,15 @@
         </div>
         <div class="help-collapsible-container">
             <div class="help-collapsible-group slide-up" role="group" ref="group10">
-                <ul id="helpFileFormat" class="collapse">
-                    <li>The output file is a comma delimited file that should be directly readable by Excel, LibreOffice, OpenOffice or similar products.</li>
-                    <li>When opening the file, you may need to set the delimeter (Separator) to a comma only.  Uncheck options for separating by Tab, Semicolon, etc...</li>
-                    <li>File layout is as follows:
+                <ul id="helpFileFormat" class="collapse help-left">
+                    <li>The output file is a comma delimited file that should be directly readable by Excel, LibreOffice,
+                        OpenOffice or similar products.  When opening the file, you may need to set the delimeter (Separator)
+                        to a comma only.  Uncheck options for separating by Tab, Semicolon, etc...  File layout is as follows:
+                        <br/><br/>
                         <ul>
-                            <li>Header row for cruise info (date, person, cruise name, BAF, etc...</li>
-                            <li>One row for cruise data.</li>
-                            <li>Header row for plot specific data (plotid, plotnum, accuracy, latitude, longitude, etc...</li>
-                            <li>One row for EACH PLOT.  Note that some fields may not currently be used.</li>
-                            <li>Header row for tree data.</li>
-                            <li>One row for EACH TREE.  Rows are ordered by plotnum.</li>
-                            <li>Most fields should be self explanatory.  Accuracy, latitude and longitude are geolocation
-                                data and should reflect the accuracy and coordinates of each plot.
-                            </li>
-                        </ul>
+                            <li><span class="help-item">Headers -</span> are included for cruise info, plot info, and tree data.</li>
+                            <li><span class="help-item">Data -</span> one row for cruise data, one row for each plot and one row for each tree.</li>
+                         </ul>
                     </li>
                 </ul>
             </div>
@@ -204,59 +194,9 @@
         <div class="help-collapsible-container">
             <div class="help-collapsible-group  slide-up" role="group" ref="group11">
                 <ul id="helpRelease" class="collapse">
-                    <li>Version 1.3.2
-                        <ul>
-                            <li>Added statistics popup on cruise screen.</li>
-                            <li>Renamed apple-touch-icon.png to icon.png.</li>
-                            <li>Added manifest.json for better homescreen experience.</li>
-                        </ul>
-                    </li>
-                    <li>Version 1.3.0
-                        <ul>
-                            <li>Disabled init parameter for field configuration since it is not implemented.</li>
-                            <li>User interface enhancements.</li>
-                            <li>Add support for loading from file URL.</li>
-                            <li>Fix Firefox/localstorage bug during initial config.</li>
-                            <li>Add service worker support.</li>
-                            <li>Update to Jquery Mobile 1.4.5.</li>
-                            <li>Update to Knockout.js 3.4.0.</li>
-                        </ul>
-                    </li>
-                    <li>Version 1.2.3
-                        <ul>
-                            <li>Reworked speech input routines - see help for new instructions.</li>
-                        </ul>
-                    </li>
-                    <li>Version 1.2.0122.1
-                        <ul>
-                            <li>Fixed problem with sticky cache files.</li>
-                            <li>Add prompt for user to reload after version update.</li>
-                        </ul>
-                    </li>
-                    <li>Version 1.2.0115.1
-                        <ul>
-                            <li>Incorporated jquery-mobile V1.4.0 for improved performance.</li>
-                            <li>Incorporated knockout.js V3.0.0 for improved performance.</li>
-                            <li>Config - fixed species delete button overlap for small mobile devices.</li>
-                            <li>Reworked button markup to take advantage of JQM 1.4.0 performance enhancements.</li>
-                            <li>Fixed error with default cruiseparms not being saved to localstorage.</li>
-                            <li>Removed fastclick.js library.</li>
-                        </ul>
-                    </li>
-                    <li>Version 1.1.0925.2
-                        <ul>
-                            <li>Removed default sawlog value to fix Firefox tab issue.</li>
-                            <li>Added ability to configure field defaults for each cruise as opposed to globally.</li>
-                            <li>Added multi product mode.</li>
-                            <li>Add ability to download data as a file for browsers that support BLOB URLs.</li>
-                            <li>Bigger dialog close buttons.</li>
-                            <li>Add workaround for IOS 7 Safari bugs related to page navigation.</li>
-                        </ul>
-                    </li>
-                    <li>Version 1.1.1023.1
-                        <ul>
-                            <li>Workaround for Firefox indexedDB index creation bug.</li>
-                        </ul>
+                    <li>
+                        <h4>Version 2.0</h4>
+                        Initial release
                     </li>
                 </ul>
             </div>
@@ -268,7 +208,7 @@
         <div class="help-collapsible-container">
             <div class="help-collapsible-group  slide-up" role="group" ref="group12">
                 <ul id="helpAbout" class="collapse">
-                    <li>Version: 1.3.1</li>
+                    <li>Version: 2.0</li>
                     <li>OpenCruise is open source and licensed under the :
                         <a href="http://www.gnu.org/copyleft/gpl.html">GNU Public License Version 3.</a></li>
                     <li>Developed by <a href="mailto:jcrivard@mtu.edu">Jim Rivard.</a></li>
