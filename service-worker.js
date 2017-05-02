80f971f1297ca37724e1d4952f56cea7eedd9bbc
jcrivard <jcrivard@mtu.edu>
2017-05-02 06:20:59 -0400

    Production Build

--- service-worker.js
+++ service-worker.js
@@ -4,7 +4,7 @@ Licensed under the GNU Public License Version 3:
 http://www.gnu.org/copyleft/gpl.html
 credit to https://ole.michelsen.dk/blog/making-an-offline-webapp-with-service-workers.html
 */
-var CACHE_VERSION = 'v2.0.3';
+var CACHE_VERSION = 'v2.0.0';
 
 self.addEventListener('install', function (event) {
     event.waitUntil(
@@ -12,14 +12,14 @@ self.addEventListener('install', function (event) {
             return cache.addAll([
                 'manifest.json',
                 'static/css/material-surface.css',
-                'static/css/app.a92291b07898e74d4075cab15ba4a9a5.css',
+                'static/css/app.f45d4bc629282fbfb76bed3175d2c290.css',
                 'static/fonts/materialicons-regular.570eb83.woff2',
                 'static/images/biltmore.png',
                 'static/images/icon-144.png',
                 'static/images/icon.png',
-                'static/js/app.5451b715ba8b89482016.js',
+                'static/js/app.61f9689251086ab9e186.js',
                 'static/js/vendor.a0d197278e968aaa56ac.js',
-                'static/js/manifest.58061bb2c71cdfa609c1.js',
+                'static/js/manifest.be15891008425304d696.js',
                 'index.html']);
         }).then(function() {
             console.log('[ServiceWorker] Install for version', CACHE_VERSION, 'Time:', Date());
